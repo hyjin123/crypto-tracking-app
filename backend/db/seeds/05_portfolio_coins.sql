d10ab123a017e644580ee61afb15a1c58256322b
Sean <seanhoyeonjin@gmail.com>
2021-11-23 18:56:44 -0500

    managed to set Holdings state with api info

--- backend/db/seeds/05_portfolio_coins.sql
+++ backend/db/seeds/05_portfolio_coins.sql
@@ -1,8 +1,8 @@
 INSERT INTO portfolio_coins (portfolio_id, coin_id, holdings)
 VALUES
-(3, 5, 100),
+(3, 1, 100),
 (2, 2, 200),
 (1, 1, 50),
 (1, 2, 2),
 (2, 5, 45),
-(3, 4, 55);
+(3, 1, 55);
--- myapp/src/components/Portfolio/CoinTable.js
+++ myapp/src/components/Portfolio/CoinTable.js
@@ -12,20 +12,6 @@ import {
 import { makeStyles } from "@mui/styles";
 import axios from "axios";
 
-// dummy data for now
-// const holdings = [
-//   {
-//     id: 1,
-//     coin: "Bitcoin",
-//     price: 100,
-//     volume: 20000300,
-//     change: 3,
-//     market: 120000,
-//     holdings: 2,
-//     pnl: 3,
-//   },
-// ];
-
 // this is a makeStyles hook (Custom css)
 const useStyles = makeStyles({
   cell: {
@@ -37,22 +23,51 @@ const useStyles = makeStyles({
 
 const CoinTable = (props) => {
   const [holdings, setHoldings] = useState([]);
-  const { firstName, lastName, userId } = props;
+  const [userId, setUserId] = useState(0);
+  // const { firstName, lastName, userId } = props;
   const classes = useStyles();
 
+  // retrieve the token from local storage, if empty string, you need to logged in.
+  const token = localStorage.getItem("jwtToken");
+
+  // authenticates the user and gets the user's infomation and store them in state
   useEffect(() => {
-    if (userId !== 0) {
-      axios
-        .get(`/api/portfolio/coin?userId=${userId}`, {}) //whenever you make a get request, you send the body/data as query string
-        .then((res) => {
-          setHoldings(res.data.holdings);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
+    axios
+      .get("/isUserAuth", {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((res) => {
+        const user = res.data.user;
+        setUserId(user.id);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    Promise.all([
+      axios.get(`/api/portfolio/coin?userId=${userId}`, {}),
+      axios.get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      ),
+    ]).then((all) => {
+      const holdings = all[0].data.holdings;
+      const resultsArray = [];
+      const data = all[1].data;
+      for (let i = 0; i < holdings?.length; i++) {
+        for (let j = 0; j < data?.length; j++) {
+          if (holdings[i].name == data[j].name) {
+            resultsArray.push(data[j]);
+          }
+        }
+      }
+      setHoldings(resultsArray);
+    });
   }, [userId]);
-  console.log(holdings)
+
   return (
     <>
       <Typography
@@ -76,8 +91,8 @@ const CoinTable = (props) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {holdings.map((coin) => (
-            <TableRow key={coin.coin_id} className="table-row">
+          {holdings.map((coin, index) => (
+            <TableRow key={index} className="table-row">
               <TableCell className={classes.cell}>{coin.name}</TableCell>
               <TableCell className={classes.cell}>{coin.price}</TableCell>
               <TableCell className={classes.cell}>{coin.volume}</TableCell>
